acf3d67ac253903159d0ea959addf38e5fca7fb3
Jackson Wozniak <jjw30@comcast.net>
2024-10-17 13:02:01 -0400

    initial files

---
+++ backend/src/main/java/internal/api/backend/MMATournamentApplication.java
@@ -0,0 +1,13 @@
+package internal.api.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMATournamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MMATournamentApplication.class, args);
+    }
+
+}
